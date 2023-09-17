213a849fc50af354687562b68594b12d1e02370a
Marchenko Yaroslava <fortunefame1710@gmail.com>
2023-09-17 14:41:05 +0200

    Update readable.js

--- hw-6/readable.js
+++ hw-6/readable.js
@@ -1,5 +1,5 @@
 const { Readable } = require('stream');
-
+ 
 class MyReadable extends Readable {
     constructor(dataStr, options) {
         super(options);
@@ -8,19 +8,19 @@ class MyReadable extends Readable {
         this.leftover = '';
         this.chunkCount = 0;
     };
-
+ 
     _read() {
         const chunkSize = this.readableHighWaterMark;
         let chunk = this.dataStr.substring(this.dataIndex, this.dataIndex + chunkSize - this.leftover.length);
-
+ 
         if (this.leftover) {
             chunk = this.leftover + chunk;
             this.leftover = '';
         };
-
+ 
         const trailingSpaces = /\s+$/;
         const spaceMatch = chunk.match(trailingSpaces);
-
+ 
         if (spaceMatch) {
             if (spaceMatch.index === 0) {
                 chunk = '';
@@ -30,34 +30,24 @@ class MyReadable extends Readable {
                 chunk = chunk.substring(0, spaceMatch.index);
             };
         };
-
+ 
         this.dataIndex += chunkSize;
-
+ 
         if (chunk) {
             this.chunkCount++;
             console.log(`Chunk № ${this.chunkCount}:`, `|${chunk}|`);
         };
-
+ 
         this.push(chunk);
-
+ 
         if (this.dataIndex >= this.dataStr.length && !this.leftover) {
             this.push(null);
         };
     };
 };
-
-// const dataToRead = '   dfgdfgdfg                 dfgf  fdg dsfg dfg dfg dfg dfg   ffdfffffffffffff ';
-const dataToRead = '             ';
-
-// const dataToReadArray = [
-//     "Text line 1 ",
-//     " Text line 2",
-//     "Text line 3 ",
-//     "     1      ",
-//     "123"
-// ];
-// const dataToRead = dataToReadArray.join('');
-
+ 
+const dataToRead = '   dfgdfgdfg                 dfgf  fdg dsfg dfg dfg dfg dfg   ffdfffffffffffff ';
+ 
 const reader = new MyReadable(dataToRead, { highWaterMark: 8 });
 reader.on('data', (data) => {}); 
 reader.on('end', () => {
